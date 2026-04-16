720228738c693dfa7df7b9c0dbf10d16fdfb8418
sharp1 <cundiffterry20@gmail.com>
2026-04-16 17:44:14 -0400

    closed the wide open access for ssh for Checkov test

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,15 +13,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "bad_sg" {
-  name        = "bad-sg"
-  description = "Intentionally insecure security group for pipeline testing"
+variable "trusted_ssh_cidr" {
+  description = "Trusted CIDR allowed to access SSH"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+resource "aws_security_group" "restricted_sg" {
+  name        = "restricted-sg"
+  description = "Restricted SSH access for pipeline testing"
 
   ingress {
-    description = "SSH open to the world"
+    description = "SSH restricted to trusted CIDR"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.trusted_ssh_cidr]
   }
 }
