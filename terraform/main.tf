be4bf00ff1519e2c462c8808485fb92a50527db0
sharp1 <cundiffterry20@gmail.com>
2026-04-16 17:35:34 -0400

    Add intentionally insecure security group for Checkov test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,27 @@
 terraform {
   required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "null_resource" "example" {}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "bad_sg" {
+  name        = "bad-sg"
+  description = "Intentionally insecure security group for pipeline testing"
+
+  ingress {
+    description = "SSH open to the world"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
