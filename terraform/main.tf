1349b6eecaef7d6bf6e3cc0d6de6c64d8c29d995
sharp1 <cundiffterry20@gmail.com>
2026-04-17 13:01:34 -0400

    update to main with a s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,21 +13,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "trusted_ssh_cidr" {
-  description = "Trusted CIDR allowed to access SSH"
-  type        = string
-  default     = "10.0.0.0/24"
-}
-
-resource "aws_security_group" "restricted_sg" {
-  name        = "restricted-sg"
-  description = "Restricted SSH access for pipeline testing"
-
-  ingress {
-    description = "SSH restricted to trusted CIDR"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.trusted_ssh_cidr]
-  }
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "marquis-devsecops-pipeline-test-123456"
 }
