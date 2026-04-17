80761dd07446ae0accc4da5394ae05ab9d757f0e
sharp1 <cundiffterry20@gmail.com>
2026-04-17 13:48:55 -0400

    Update with skip comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,6 @@ provider "aws" {
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "marquis-devsecops-logs-123456"
 }
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "marquis-devsecops-logs-123456"
-}
 
 resource "aws_s3_bucket_public_access_block" "log_bucket_block_public" {
   bucket = aws_s3_bucket.log_bucket.id
@@ -84,9 +81,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 #checkov:skip=CKV2_AWS_62: Event notifications are not required for this training bucket
 #checkov:skip=CKV2_AWS_61: Lifecycle configuration is not required for this non-production training bucket
 #checkov:skip=CKV_AWS_144: Cross-region replication is not required for this non-production training bucket
-resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "marquis-devsecops-pipeline-secure-123456"
-}
 resource "aws_s3_bucket_logging" "secure_bucket_logging" {
   bucket        = aws_s3_bucket.secure_bucket.id
   target_bucket = aws_s3_bucket.log_bucket.id
